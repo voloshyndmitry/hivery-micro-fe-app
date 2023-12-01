41c9a531ca41b4a051047aaaab7a9908212fee10
Voloshyn Dmytro <voloshyndmytro@Voloshyns-MBP.lan>
2023-12-01 09:08:26 -0700

    fix/.env variables

--- vite.config.js
+++ vite.config.js
@@ -5,22 +5,18 @@ import federation from "@originjs/vite-plugin-federation";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   // from https://stackoverflow.com/questions/66389043/how-can-i-use-vite-env-variables-in-vite-config-js
-  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
-  console.log("ENV >>>", process.env.VITE_TEST)
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
   return {
     plugins: [
       react(),
       federation({
         name: "app",
         remotes: {
-          remoteApp:
-            "https://unpkg.com/hivery-shared-components@0.0.10/dist/assets/remoteEntry.js",
-          hiveryStore:
-            "https://unpkg.com/hivery-store@0.0.2/dist/assets/remoteEntry.js",
-          hiveryMicroApp1:
-            "https://unpkg.com/hivery-micro-app1@0.0.3/dist/assets/remoteEntry.js",
-          hiveryMicroApp2:
-            "https://unpkg.com/hivery-micro-app2@0.0.2/dist/assets/remoteEntry.js",
+          remoteApp: process.env.VITE_SHARED_COMPONENTS,
+          hiveryStore: process.env.VITE_HIVERY_STORE,
+          hiveryMicroApp1: process.env.VITE_HIVERY_MICRO_APP1,
+          hiveryMicroApp2: process.env.VITE_HIVERY_MICRO_APP2,
         },
         shared: ["react", "react-dom"],
       }),
